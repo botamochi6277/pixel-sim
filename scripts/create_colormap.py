660a69549b37165c8adaf281c9e15ebb1bb5a9cc
botamochi6277 <botamochi6277@icloud.com>
2024-05-16 20:56:41 +0900

    Add colormap encoding

--- scripts/create_colormap.py
+++ scripts/create_colormap.py
@@ -1,3 +1,5 @@
+import json
+
 import matplotlib.pyplot as plt
 import numpy as np
 from fire import Fire
@@ -8,7 +10,15 @@ import pixel_sim
 
 
 # https://pystyle.info/matplotlib-master-of-colormap/
-def create_colormap(color1: str, color2: str, color3: str):
+def create_colormap(
+    color1: str,
+    color2: str,
+    color3: str,
+    name="custom",
+    is_encode: bool = False,
+    is_cyclic: bool = False,
+    order: int = 4,
+):
     rgb1 = np.array(ImageColor.getcolor(color1, "RGB")) / 255
     rgb2 = np.array(ImageColor.getcolor(color2, "RGB")) / 255
     rgb3 = np.array(ImageColor.getcolor(color3, "RGB")) / 255
@@ -30,7 +40,7 @@ def create_colormap(color1: str, color2: str, color3: str):
             (1.0, rgb3[2], rgb3[2]),
         ],
     }
-    cmap = LinearSegmentedColormap("custom", cdict)
+    cmap = LinearSegmentedColormap(name, cdict)
 
     num = 101
     t = np.linspace(0.0, 1.0, num=num)
@@ -41,22 +51,118 @@ def create_colormap(color1: str, color2: str, color3: str):
 
     # print(f"rgba: {rgba}")
 
+    ncols = 2 if is_encode else 1
     fig, axes = plt.subplots(
-        nrows=2,
+        nrows=3,
+        ncols=ncols,
         sharex="col",
-        #  ncols=2, sharex="col", sharey="row", height_ratios=(1, 0.25, 0.25)
+        sharey="row",
+        height_ratios=(1, 0.25, 0.25),
     )
 
+    ax = axes[0] if not is_encode else axes[0][0]
     pixel_sim.plotting.plot_rgb_curve(
-        t2, rgba[:, 0], rgba[:, 1], rgba[:, 2], ax=axes[0], show_legend=True
+        t2, rgba[:, 0], rgba[:, 1], rgba[:, 2], ax=ax, show_legend=(not is_encode)
     )
+    ax = axes[1] if not is_encode else axes[1][0]
+    pixel_sim.plotting.plot_color_ribbon(t2, rgba[:, 0], rgba[:, 1], rgba[:, 2], ax=ax)
 
+    rgb_screen = np.vstack(
+        (
+            pixel_sim.color.apply_screen(np.ones_like(rgba[:, 0]) * 0.5, rgba[:, 0]),
+            pixel_sim.color.apply_screen(np.ones_like(rgba[:, 0]) * 0.5, rgba[:, 1]),
+            pixel_sim.color.apply_screen(np.ones_like(rgba[:, 0]) * 0.5, rgba[:, 2]),
+        )
+    ).T
+
+    ax = axes[2] if not is_encode else axes[2][0]
     pixel_sim.plotting.plot_color_ribbon(
-        t2, rgba[:, 0], rgba[:, 1], rgba[:, 2], ax=axes[1]
+        t2, rgb_screen[:, 0], rgb_screen[:, 1], rgb_screen[:, 2], ax=ax
     )
 
-    axes[0].set_xlim(([-1.1, 1.1]))
+    ax.set_xlim(([-1.1, 1.1]))
 
+    if is_encode:
+        if is_cyclic:
+            cyclic_params = pixel_sim.analysis.compute_cyclic_param(
+                rgba[:, 0], rgba[:, 1], rgba[:, 2], order=order
+            )
+            rgb_fit = np.vstack(
+                pixel_sim.analysis.compute_cyclic_color(cyclic_params)
+            ).T
+
+            data = {
+                "name": name,
+                "type": "cyclic",
+                "red": {"f": cyclic_params[0].f, "z": cyclic_params[0].z},
+                "green": {"f": cyclic_params[1].f, "z": cyclic_params[1].z},
+                "blue": {"f": cyclic_params[2].f, "z": cyclic_params[2].z},
+            }
+        else:
+            poly_params = pixel_sim.analysis.compute_poly_param(
+                rgba[:, 0], rgba[:, 1], rgba[:, 2], order=order
+            )
+            rgb_fit = np.vstack(
+                pixel_sim.analysis.compute_poly_color(poly_params, num=num)
+            ).T
+
+            data = {
+                "name": name,
+                "type": "poly",
+                "red": poly_params[0].tolist(),
+                "green": poly_params[1].tolist(),
+                "blue": poly_params[2].tolist(),
+            }
+
+        json_name = f"{name}.json"
+        with open(json_name, "w") as fp:
+            json.dump(data, fp)
+        print(f"{json_name} was written")
+        ax = axes[0][1]
+
+        pixel_sim.plotting.plot_rgb_curve(
+            t2,
+            rgb_fit[:, 0],
+            rgb_fit[:, 1],
+            rgb_fit[:, 2],
+            ax=ax,
+            show_legend=is_encode,
+        )
+
+        ax = axes[1][1]
+        pixel_sim.plotting.plot_color_ribbon(
+            t2,
+            rgb_fit[:, 0],
+            rgb_fit[:, 1],
+            rgb_fit[:, 2],
+            ax=ax,
+        )
+
+        rgb_screen2 = np.vstack(
+            (
+                pixel_sim.color.apply_screen(
+                    np.ones_like(rgb_fit[:, 0]) * 0.5, rgb_fit[:, 0]
+                ),
+                pixel_sim.color.apply_screen(
+                    np.ones_like(rgb_fit[:, 0]) * 0.5, rgb_fit[:, 1]
+                ),
+                pixel_sim.color.apply_screen(
+                    np.ones_like(rgb_fit[:, 0]) * 0.5, rgb_fit[:, 2]
+                ),
+            )
+        ).T
+
+        ax = axes[2][1]
+        pixel_sim.plotting.plot_color_ribbon(
+            t2,
+            rgb_screen2[:, 0],
+            rgb_screen2[:, 1],
+            rgb_screen2[:, 2],
+            ax=ax,
+        )
+        ax.set_xlim(([-1.1, 1.1]))
+
+    plt.tight_layout()
     plt.show()
 
 
